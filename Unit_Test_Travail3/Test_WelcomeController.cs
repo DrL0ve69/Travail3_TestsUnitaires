bdbce0e299dd34d128385c7d05cc5ee58002e611
Francis Olivier Houle <francis.o.houle@outlook.com>
2025-05-07 10:19:58 -0400

    Test unitaires Exercice 1

---
+++ Unit_Test_Travail3/Test_WelcomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Travail3_TestsUnitaires.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Unit_Test_Travail3
+{
+    public class Test_WelcomeController
+    {
+        [Fact]
+        public void Test_WelcomeDefault()
+        {
+            // Arrange
+            var welcomeController = new WelcomeController();
+            // Act
+            var result = welcomeController.WelcomeDefault();
+            // Assert
+            var viewResult = Assert.IsType<ContentResult>(result);
+        }
+
+        [Fact]
+        public void Test_WelcomeName()
+        {
+            // Arrange
+            var welcomeController = new WelcomeController();
+            // Act
+            var result = welcomeController.WelcomeName("Bob", "Cashflow");
+            //Assert    
+            var viewResult = Assert.IsType<ContentResult>(result);
+            Assert.Equal("Salut Bob Cashflow ! Bienvenue dans ma nouvelle application!", viewResult.Content);
+        }
+    }
+}
